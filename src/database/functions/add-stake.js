4af1b91d4926bc8782ec0ce7511cc5312824bdf4
unknown <Kelly Owoju>
2023-12-27 18:11:00 +0100

    pulling from parshva

--- src/database/functions/add-stake.js
+++ src/database/functions/add-stake.js
@@ -21,6 +21,18 @@ const addStake = async ({
     durationInMonths,
     stakedOn: new Date().toISOString(),
     txReference,
+  }).then(() => {
+    console.log("Data has been added");
+    const data = {
+      walletId,
+      currencyType,
+      stakedAmount,
+      apy,
+      durationInMonths,
+      stakedOn: new Date().toISOString(),
+      txReference,
+    };
+    console.log("Sent data:::", data);
   });
 };
 
