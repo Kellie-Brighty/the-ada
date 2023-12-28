d9257349ecfed0bfcec80bd2b17e3d5a715fb748
unknown <Kelly Owoju>
2023-12-28 14:08:38 +0100

    fixed mobile not showing

--- src/pages/stake.jsx
+++ src/pages/stake.jsx
@@ -10,7 +10,7 @@ import {
   resolvePaymentKeyHash,
   KoiosProvider,
 } from "@meshsdk/core";
-import { scriptAddr, contract } from "./logic/contract";
+// import { scriptAddr, contract } from "./logic/contract";
 // const { scriptAddr, contract } = require("./logic/contract");
 import addStake from "../database/functions/add-stake";
 import { useNavigate } from "react-router-dom";
@@ -19,7 +19,9 @@ async function Deposit(w, quantity, stakeAddress, currencyType, duration) {
   let userAddress = await w.getUsedAddress();
   let userAddressStr = userAddress.to_bech32("addr").toString();
   // console.log("user address:::", userAddressStr)
-  let contractAddress = scriptAddr.toString();
+  let contractAddress =
+    "addr1w85vjne65nh3zfp9ndf6xnk02uxkzlsdtczsu7pxw8gatuqe9vpg8";
+  console.log("contract address:::", contractAddress);
   const userWalletKeyhash = resolvePaymentKeyHash(userAddressStr);
   let tadaUnit =
     "9eaed3f99f5c9da1695acaf2542cd6b9f3ef18bbf94dd3f77d17f9cb54414441";
@@ -112,36 +114,36 @@ export const Deposit18 = async (
 
 // txBuilder = new TxBuilder("mainnet")
 
-export const Withdraw = async (name) => {
-  //  tx
-  let w = await BrowserWallet.enable(name);
-  let userAddress = await w.getUsedAddress();
-  let userAddressStr = userAddress.to_bech32("addr").toString();
-  let contractAddress = scriptAddr.toString();
-  const userWalletKeyhash = resolvePaymentKeyHash(userAddressStr);
-  let tadaUnit =
-    "9eaed3f99f5c9da1695acaf2542cd6b9f3ef18bbf94dd3f77d17f9cb54414441";
+// export const Withdraw = async (name) => {
+//   //  tx
+//   let w = await BrowserWallet.enable(name);
+//   let userAddress = await w.getUsedAddress();
+//   let userAddressStr = userAddress.to_bech32("addr").toString();
+//   let contractAddress = scriptAddr.toString();
+//   const userWalletKeyhash = resolvePaymentKeyHash(userAddressStr);
+//   let tadaUnit =
+//     "9eaed3f99f5c9da1695acaf2542cd6b9f3ef18bbf94dd3f77d17f9cb54414441";
 
-  // To-do: move the api key used in fetching utxo's to .env file
+//   // To-do: move the api key used in fetching utxo's to .env file
 
-  const blockchainProvider = new KoiosProvider(
-    "api",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZGRyIjoic3Rha2UxdTlseXFrYWQ0bDdsOG1lanZsY3I4bGwza21rdm1wamhkZGh2dGdmeGcwMjk0bmN4azdqcTciLCJleHAiOjE3MzQ5NzI1NTYsInRpZXIiOjEsInByb2pJRCI6InRoZWFkYXBybyJ9.rmVUtYYYaL18XaRP1OTecIg7I_dw4U75blG8avt2Wzs"
-  );
-  const assetHex = "54414441";
-  const policyId = "9eaed3f99f5c9da1695acaf2542cd6b9f3ef18bbf94dd3f77d17f9cb";
-  const utxos = await blockchainProvider.fetchAddressUTxOs(
-    contractAddress,
-    policyId + assetHex
-  );
-  const tx = new Transaction({ initiator: w });
+//   const blockchainProvider = new KoiosProvider(
+//     "api",
+//     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZGRyIjoic3Rha2UxdTlseXFrYWQ0bDdsOG1lanZsY3I4bGwza21rdm1wamhkZGh2dGdmeGcwMjk0bmN4azdqcTciLCJleHAiOjE3MzQ5NzI1NTYsInRpZXIiOjEsInByb2pJRCI6InRoZWFkYXBybyJ9.rmVUtYYYaL18XaRP1OTecIg7I_dw4U75blG8avt2Wzs"
+//   );
+//   const assetHex = "54414441";
+//   const policyId = "9eaed3f99f5c9da1695acaf2542cd6b9f3ef18bbf94dd3f77d17f9cb";
+//   const utxos = await blockchainProvider.fetchAddressUTxOs(
+//     contractAddress,
+//     policyId + assetHex
+//   );
+//   const tx = new Transaction({ initiator: w });
 
-  // withdraw function with error
-  await tx.redeemValue({ value: utxos[0], script: contract, datum: utxos[0] });
+//   // withdraw function with error
+//   await tx.redeemValue({ value: utxos[0], script: contract, datum: utxos[0] });
 
-  const unsignedTx = await tx.build();
-  const signedTx = await w.signTx(unsignedTx);
-  const txHash = await w.submitTx(signedTx);
+//   const unsignedTx = await tx.build();
+//   const signedTx = await w.signTx(unsignedTx);
+//   const txHash = await w.submitTx(signedTx);
 
-  console.log("txHash", txHash);
-};
+//   console.log("txHash", txHash);
+// };
